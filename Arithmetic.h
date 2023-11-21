e4a514e91e13c2bacab261b242f40bf46f92c849
PriyanshuRao-code <learningmodeon2.0@gmail.com>
2023-11-21 16:51:50 +0530

    If-else to switch

--- Arithmetic.h
+++ Arithmetic.h
@@ -1,37 +1,57 @@
+#include <stdio.h>
 #include <math.h>
 void Arithmetic();
 void Arithmetic_explain(){
     printf("Here are the functions allowed here and the format is as:\n");
-    printf("Addition: a + b\n");
-    printf("Subtraction: a - b\n");
-    printf("Multiplication: a * b\n");
-    printf("Division: a / b\n");
-    printf("Power: a ^ b\n");
+    printf("Addition: a+b\n");
+    printf("Subtraction: a-b\n");
+    printf("Multiplication: a*b\n");
+    printf("Division: a/b\n");
+    printf("Power: a^b\n");
     printf("Root of the function can be calculated as: a ^ 0.5\n");
     Arithmetic(); 
 }
-Arithmetic(){
-    float a,b;
-    char c;
+void Arithmetic(){
+    float a=0,b=0;
+    char c='A';
     printf("Enter the arithmetic operation:");
-    scanf("%d %c %d",&a,&c,&b);
-    if(c=='+'){
-        printf("%f",a+b);
+    scanf("%f%c%f",&a,&c,&b);
+    if (c==' ') {
+        printf("Don't use spaces in between calculations\n");
+        //printf("Enter the arithmetic operation:");
+        while(getchar()!='\n');
+        Arithmetic();
+        //scanf("%f%c%f",&a,&c,&b);
     }
-    else if(c=='-'){
-        printf("%f",a-b);
+    switch(c){
+        case '+':
+            printf("%f",a + b);
+            break;
+        case '-':
+            printf("%f",a-b);
+            break;
+        case '*':
+            printf("%f",a*b);
+            break;
+        case '^':
+            printf("%f",pow(a,b));
+            break;
+        case '/':
+            if(b!=0){
+            printf("%f",a/b);
+            }
+            else{
+                printf("Can't divide by 0");
+            }
+            break;
+        case ' ':
+            printf("Don't use spaces in between for calculation\n");
+            Arithmetic();
+            break;
+        default:
+            printf("Please refer to the functions:\n");
+            Arithmetic_explain();
+            break;
     }
-    else if(c=='*'){
-        printf("%f",a*b);
-    }
-    else if(c=='^'){
-        printf("%f",pow(a,b));
-    }
-    else if(c=='/'){
-        printf("%f",a/b);
-    }
-    else {
-        printf("Please refer to the functions:\n");
-        Arithmetic_explain();
-    }
+    
 }
--- Project.c
+++ Project.c
@@ -7,68 +7,82 @@ int main(){
   int n,m;
   printf("Welcome to the world of calculation\n\n");
   printf("Which type of calculations would you like to perform:\n");
-  printf("1. Arithmetic operations\n
-          2. Complex number operations\n
-          3. Matrix operations\n
-          4. Co-ordination operations\n");
+  printf("1. Arithmetic operations\n");
+  printf("2. Complex number operations\n");
+  printf("3. Matrix operations\n");
+  printf("4. Co-ordination operations\n");
   scanf("%d",&n);
-  printf("1. Proceed\n
-          2. Explain operations\n");
+
+  printf("1. Proceed\n");
+  printf("2. Explain operations\n");
   scanf("%d",&m);
 
-
   //Just Executing the the above by sending the calculation to respective functions in their respective header files
-  if(n==1){
-    if(m==1){
-      Arithmetic();
-    }
-    else if(m==2){
-      Arithmetic_explain();
-    }
-    else {
-      printf("Error\n
-              Enter 1(Proceed) or 2(Explain) only\n");
-    }
-  }
-  else if(n==2){
-    if(m==1){
-      Complex();
-    }
-    else if(m==2){
-      Complex_explain();
-    }
-    else {
-      printf("Error\n
-              Enter 1(Proceed) or 2(Explain) only\n");
-    }
-  }
-  else if(n==3){
-    if(m==1){
-      Matrix();
-    }
-    else if(m==2){
-      Matrix_explain();
-    }
-    else {
-      printf("Error\n
-              Enter 1(Proceed) or 2(Explain) only\n");
-    }
-  }
-  else if(n==4){
-    if(m==1){
-      Co_ordination();
-    }
-    else if(m==2){
-      Co_ordination_explain();
-    }
-    else {
-      printf("Error\n
-              Enter 1(Proceed) or 2(Explain) only\n");
-    }
-  }
-  else{
-    printf("Error\n
-            Enter correct value for operations\n");
+  switch(n){
+    case 1:
+      switch(m){
+        case 1:
+          Arithmetic();
+          break;
+        case 2:
+          Arithmetic_explain();
+          break;
+        default:
+          printf("Error\n");
+          printf("Enter 1(Proceed) or 2(Explain) only\n");
+          break;
+      }
+      break;
+
+    case 2:
+      switch(m){
+        case 1:
+          //Complex();
+          break;
+        case 2:
+          //Complex_explain();
+          break;
+        default:
+          printf("Error\n");
+          printf("Enter 1(Proceed) or 2(Explain) only\n");
+          break;
+      }
+      break;
+    
+    case 3:
+      switch(m){
+        case 1:
+          //Matrix();
+          break;
+        case 2:
+          //Matrix_explain();
+          break;
+        default:
+          printf("Error\n");
+          printf("Enter 1(Proceed) or 2(Explain) only\n");
+          break;
+      }
+      break;
+
+    case 4:
+      switch(m){
+        case 1:
+          //Co_ordination();
+          break;
+        case 2:
+          //Co_ordination_explain();
+          break;
+        default:
+          printf("Error\n");
+          printf("Enter 1(Proceed) or 2(Explain) only\n");
+          break;
+      }
+      break;
+    
+    default:
+    printf("Error\n");
+    printf("Enter correct value for operations\n");
+    break;
   }
   return 0;
 }
