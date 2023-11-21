59e706d9d02effa5ac9823c2de5dda50c461f41f
PriyanshuRao-code <learningmodeon2.0@gmail.com>
2023-11-22 02:12:34 +0530

    Functions added for smooth operation for invalid inputs

--- Project.c
+++ Project.c
@@ -3,8 +3,12 @@
 #include "Complex.h"
 #include "Matrix.h"
 #include "Co_ordination.h"
-int main(){
-  int n,m;
+int operation();
+int explanation();
+
+
+int operation(){
+  int n;
   printf("Welcome to the world of calculation\n\n");
   printf("Which type of calculations would you like to perform:\n");
   printf("1. Arithmetic operations\n");
@@ -12,10 +16,38 @@ int main(){
   printf("3. Matrix operations\n");
   printf("4. Co-ordination operations\n");
   scanf("%d",&n);
+  if(n!=1 && n!=2 && n!=3 && n!=4) {
+    printf("\nError\n");
+    printf("Enter correct value for operations\n\n");
+    return -1;
+  }
+  return n;
+}
 
+
+  int explanation(){
+  int m;
   printf("1. Proceed\n");
   printf("2. Explain operations\n");
   scanf("%d",&m);
+
+  if(m!=1 && m!=2){
+    printf("\nError\n");
+    printf("Enter 1(Proceed) or 2(Explain) only\n\n");
+
+    return -1;
+  }
+  return m;
+}
+
+int main(){
+  int n,m;
+  n=operation();
+  if(n==-1) n=operation();
+
+  m=explanation();
+  if (m==-1) m=explanation();
+
 
   //Just Executing the the above by sending the calculation to respective functions in their respective header files
   switch(n){
@@ -27,10 +59,7 @@ int main(){
         case 2:
           Arithmetic_explain();
           break;
-        default:
-          printf("Error\n");
-          printf("Enter 1(Proceed) or 2(Explain) only\n");
-          break;
+        //No default needed covered earlier.
       }
       break;
 
@@ -42,10 +71,7 @@ int main(){
         case 2:
           //Complex_explain();
           break;
-        default:
-          printf("Error\n");
-          printf("Enter 1(Proceed) or 2(Explain) only\n");
-          break;
+        //No default needed covered earlier.
       }
       break;
     
@@ -57,10 +83,7 @@ int main(){
         case 2:
           //Matrix_explain();
           break;
-        default:
-          printf("Error\n");
-          printf("Enter 1(Proceed) or 2(Explain) only\n");
-          break;
+        //No default needed covered earlier.
       }
       break;
 
@@ -72,17 +95,10 @@ int main(){
         case 2:
           //Co_ordination_explain();
           break;
-        default:
-          printf("Error\n");
-          printf("Enter 1(Proceed) or 2(Explain) only\n");
-          break;
+        //No default needed covered earlier.
       }
       break;
-    
-    default:
-    printf("Error\n");
-    printf("Enter correct value for operations\n");
-    break;
+    //No default needed covered earlier.
   }
   return 0;
 }
