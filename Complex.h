113e8d62c8b5e34067c807188f6d56ff13a33359
PriyanshuRao-code <learningmodeon2.0@gmail.com>
2023-12-10 20:32:05 +0530

    Complex +- rectification

--- Complex.h
+++ Complex.h
@@ -16,7 +16,7 @@ void c_con();
 void c_mag();
 
 void Complex_explain(){
-    printf("\n\nNOTE: Enter the input in a+bi format only [For negative use a+-bi](without using brackets)\n");
+    printf("\n\nNOTE: Enter the input in a+bi or a-bi format only\n");
     printf("Enter a: addition\n");
     printf("Enter s: subtraction\n");
     printf("Enter m: multiplication\n");
@@ -57,9 +57,13 @@ void Complex(){
     }
 }
 complex input(){
+    char b;
     complex c;
     printf("Enter the complex number: ");
-    scanf("%f+%fi",&c.real,&c.img);
+    scanf("%f%c%fi",&c.real,&b,&c.img);
+    if (b=='-'){
+        c.img=(-1)*c.img;
+    }
     return c;
 }
 void c_add(){
@@ -71,11 +75,18 @@ void c_add(){
         c[i]=input();
     }
     complex result;
+    result.real=0;
+    result.img=0;
     for(int i=0;i<n;i++){
         result.real+=c[i].real;
         result.img+=c[i].img;
     }
-    printf("The result(aprrox upto 2 digits) is: %.2f+i%.2f",result.real,result.img);
+    //For printing purpose only [result.img is also changed]
+    char s = result.img>=0?'+':'-';
+    if(s=='-'){
+        result.img=-1*result.img;
+    }
+    printf("The result(aprrox upto 2 digits) is: %.2f%c%.2fi",result.real,s,result.img);
 }
 void c_sub(){
     int n;
@@ -86,35 +97,63 @@ void c_sub(){
         c[i]=input();
     }
     complex result;
+    result.real=0;
+    result.img=0;
     for(int i=0;i<n;i++){
         int sign = i==0?1:-1;
         result.real+=sign*c[i].real;
         result.img+=sign*c[i].img;
     }
-    printf("The result(aprrox upto 2 digits) is: %.2f+%.2fi",result.real,result.img);
+    //For printing purpose only [result.img is also changed]
+    char s = result.img>=0?'+':'-';
+    if(s=='-'){
+        result.img=-1*result.img;
+    }
+    printf("The result(aprrox upto 2 digits) is: %.2f%c%.2fi",result.real,s,result.img);
 }
 void c_mul(){
     complex c1,c2,result;
     c1=input();
     c2=input();
+    result.real=0;
+    result.img=0;
     result.real=c1.real*c2.real-c1.img*c2.img;
     result.img=c1.real*c2.img+c1.img*c2.real;
-    printf("Multiplication (approx till 2 digits): %.2f+%.2fi",result.real,result.img);
+    //For printing purpose only [result.img is also changed]
+    char s = result.img>=0?'+':'-';
+    if(s=='-'){
+        result.img=-1*result.img;
+    }
+    printf("Multiplication (approx till 2 digits): %.2f%c%.2fi",result.real,s,result.img);
 }
 void c_div(){
     complex c1,c2,result;
     c1=input();
     c2=input();
+    result.real=0;
+    result.img=0;
     result.real=(c1.real*c2.real+c1.img*c2.img)/(pow(c2.real,2)+pow(c2.img,2));
     result.img=(c1.img*c2.real-c1.real*c2.img)/(pow(c2.real,2)+pow(c2.img,2));
-    printf("Division (approx till 2 digits): %.2f+%.2fi",result.real,result.img);
+    //For printing purpose only [result.img is also changed]
+    char s = result.img>=0?'+':'-';
+    if(s=='-'){
+        result.img=-1*result.img;
+    }
+    printf("Division (approx till 2 digits): %.2f%c%.2fi",result.real,s,result.img);
 }
 void c_con(){
     complex c,result;
     c=input();
+    result.real=0;
+    result.img=0;
     result.real=c.real;
     result.img=(-1)*c.img;
-    printf("Conjugate (approx till 2 digits): %.2f+%.2fi",result.real,result.img);
+    //For printing purpose only [result.img is also changed]
+    char s = result.img>=0?'+':'-';
+    if(s=='-'){
+        result.img=-1*result.img;
+    }
+    printf("Conjugate (approx till 2 digits): %.2f%c%.2fi",result.real,s,result.img);
 }
 void c_mag(){
     complex c;
